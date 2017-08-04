3f04f4f6198c5e693c8794a3f0963bb6c62a469e
kitizl <nbalasub@ucsc.edu>
2017-08-05 00:36:42 +0530

    Added documentation and comments

--- baseConverter.py
+++ baseConverter.py
@@ -3,22 +3,35 @@ from functools import reduce
 import sys
 
 def switch(base):
+    """This function returns a list of possible digits depending on the base that is
+    passed as an argument to the function"""
     return [i for i in "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"][:base]
 
 def decToBase(num,base):
+    """This function takes in two arguments - a number and the target base - and converts
+    the number from it's decimal form to it's target base"""
     if num==0:
         return ''
     else:
         quo = num//base
         rem = num%base
+        #a recursion call recursively finds the quotient and remainder and stores the
+        #remainder when the quotient is sent as the argument for the recursion call
         return decToBase(quo,base)+switch(base)[rem]
 
 def baseToDec(num,base):
+    """This function takes in two arguments - a number and the base in which it is written
+    in. It then converts that number to decimal."""
+
+    #takes the number, turns it into an array, and switches the strings to numbers
     num_list = list(map(lambda x: switch(base).index(x),[i for i in num]))
+    #converts each and every one of the digits to decimal
     converted_list = [num_list[i]*base**(len(num_list)-1-i) for i in range(len(num_list))]
+    #returns the sum of all the numbers in the list
     return reduce(lambda x,y:x+y,converted_list)
 
 def helptext():
+    """Prints the helptext"""
     return """
 
     Usage:
@@ -39,14 +52,21 @@ try:
         fromBase = int(sys.argv[2])
         toBase = int(sys.argv[3])
 
+        #to avoid any lunatics from entering invalid bases
         if toBase == 1 or toBase == 0 or fromBase == 1 or fromBase == 0:
             print(helptext())
             sys.exit(0)
 
         number = sys.argv[4]
+        #it takes the number in the origin base, converts it into decimal
+        #and then converts it into the target base
+        #this process makes it universal for all bases (from 2 to 36) with
+        #the biggest expense being computational power
+
         print("Result :",decToBase(baseToDec(number,fromBase),toBase))
 
     else:
         print(helptext())
+
 except IndexError:
     print(helptext())
